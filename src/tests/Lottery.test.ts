aa3f9cf9b43957d9416666c88bd9854e7400d349
Kohei <ohayousagi.ac.kook0727@gmail.com>
2022-07-12 21:36:59 +0900

    main test success

--- src/tests/Lottery.test.ts
+++ src/tests/Lottery.test.ts
@@ -4,7 +4,7 @@ import Web3 from "web3";
 import compiled from "../compile";
 
 const ganache = require("ganache-cli");
-const web3 = new Web3(ganache.provider());
+const web3 = new Web3(ganache.provider({ gasLimit: 100000000000000 }));
 
 const { evm, abi } = compiled;
 const { bytecode } = evm;
@@ -14,25 +14,18 @@ const data = {
   lottery: null as any,
 };
 
-console.log("beforeEach123");
 beforeEach(async (...args) => {
-  console.log("beforeEach123");
   const accounts = await web3.eth.getAccounts();
-  console.log({ accounts });
-  const balance = await web3.eth.getBalance(accounts[0]);
-  console.log({ balance });
   const contract = new web3.eth.Contract(abi);
   const lottery = await contract
     .deploy({ data: bytecode.object })
-    .send({ from: accounts[0], gas: 100000000000000000 });
-
+    .send({ from: accounts[0], gas: 10000000 });
   data.accounts = accounts;
   data.lottery = lottery;
 });
 
 describe("Lottery", () => {
   it("deploys a contract", () => {
-    console.log({ lottery: data.lottery });
-    // assert.ok(data.lottery.options.address);
+    assert.ok(data.lottery.options.address);
   });
 });
